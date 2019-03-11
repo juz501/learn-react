444567e15afecffa3f71930889a07f2c21b15ed5
Julian Chan <julian@Julians-MacBook-Pro.local>
2019-03-11 12:12:56 +1100

    put const outside react object?

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,12 +4,16 @@ import { Helmet } from 'react-helmet';
 import H1 from '../components/H1';
 import Image from '../components/Image';
 
+const imageName = 'Pikachu.jpg';
+const imageClass = 'pokemon pikachu';
+const imageAlt = 'Pikachu';
+const imageWidth = '372px';
+const imageHeight = '372px';
+
+/**
+ * The App
+ */
 function App() {
-  const imageName = 'Pikachu.jpg';
-  const imageClass = 'pokemon pikachu';
-  const imageAlt = 'Pikachu';
-  const imageWidth = '372px';
-  const imageHeight = '372px';
   return (
     <div className="main">
       <Helmet>
