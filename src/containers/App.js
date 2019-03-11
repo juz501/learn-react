fb3d531e336b62203de92c7b4463be194499bb30
Julian Chan <julian@Julians-MacBook-Pro.local>
2019-03-11 12:37:53 +1100

    abstract into functions

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,31 +4,45 @@ import { Helmet } from 'react-helmet';
 import H1 from '../components/H1';
 import Image from '../components/Image';
 
-const imageName = 'Pikachu.jpg';
-const imageClass = 'pokemon pikachu';
-const imageAlt = 'Pikachu';
-const imageWidth = '372px';
-const imageHeight = '372px';
-const title = 'Hey There';
+
+
+function getHelmet() {
+  const title = 'Hey There';
+  const description = 'Awesome meta description';
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+    </Helmet>
+  );
+}
+function getPikachuImage() {
+  const imageName = 'Pikachu.jpg';
+  const imageClass = 'pokemon pikachu';
+  const imageAlt = 'Pikachu';
+  const imageWidth = '372px';
+  const imageHeight = '372px';
+  return (
+    <Image
+      filename={imageName}
+      className={imageClass}
+      alt={imageAlt}
+      width={imageWidth}
+      height={imageHeight}
+    />
+  );
+}
 
 /**
  * The App
  */
 function App() {
+  const title = 'Pikachu is here!';
   return (
     <div className="main">
-      <Helmet>
-        <title>{title}</title>
-        <meta name="description" content="Definitely good" />
-      </Helmet>
+      {getHelmet()}
       <H1 title={title} />
-      <Image
-        filename={imageName}
-        className={imageClass}
-        alt={imageAlt}
-        width={imageWidth}
-        height={imageHeight}
-      />
+      {getPikachuImage()}
     </div>
   );
 }
